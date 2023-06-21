3888c5eaaf42d8282dbe644eed712c61cf186c63
Ange1DA <adeanda10@alumnos.uaq.mx>
2023-06-21 16:16:21 -0600

    Update GenerarComprobante.php
    a

--- GenerarComprobante.php
+++ GenerarComprobante.php
@@ -9,44 +9,44 @@
     //FPDF
     require('fpdf.php');
 
-    $pdf = new FPDF('P','mm', array(60, 90));
+    $pdf = new FPDF('P','mm');
     $pdf->AddPage();
     $pdf->SetAutoPageBreak(true);
 
 
     //Inicio PDF
 
-    $pdf-> image('./img/sabritas.jpg', 0, 0, 10, 10);
+    $pdf-> image('./img/sabritas.jpg', 0, 0, 40, 40);
     
-    $pdf->SetFont('Arial','',2.5);
-    $pdf->SETXY(11, 1);
-    $pdf->CELL(15, 1, 'NOMBRE EMISOR', 0,);
+    $pdf->SetFont('Arial','',12);
+    $pdf->SETXY(40, 0);
+    $pdf->CELL(80, 5, 'NOMBRE EMISOR', 1,);
 
-    $pdf->SetFont('Arial','',2);
-    $pdf->SETXY(11, 2);
-    $pdf->CELL(3, 1, 'RFC: ', 0,);
-    $pdf->SETXY(14, 2);
-    $pdf->CELL(12, 1, 'RFC EMISOR', 0,);
+    $pdf->SetFont('Arial','',11);
+    $pdf->SETXY(40, 5);
+    $pdf->CELL(12, 5, 'RFC: ', 1);
+    $pdf->SETXY(52, 5);
+    $pdf->CELL(68, 5, 'RFC EMISOR', 1);
 
-    $pdf->SETXY(11, 3);
-    $pdf->CELL(15, 1, 'DOMICILIO', 0,);
+    $pdf->SETXY(40, 10);
+    $pdf->CELL(80, 5, 'DOMICILIO', 1);
 
-    $pdf->SETXY(11, 4);
-    $pdf->CELL(15, 1, 'DIRECCION', 0,);
+    $pdf->SETXY(40, 15);
+    $pdf->CELL(80, 5, 'DIRECCION', 1);
 
-    $pdf->SETXY(11, 5);
-    $pdf->CELL(15, 1, 'LOCALIDAD', 0,);
+    $pdf->SETXY(40, 20);
+    $pdf->CELL(80, 5, 'LOCALIDAD', 1);
 
-    $pdf->SetFont('Arial','',1.5);
+    $pdf->SetFont('Arial','',10);
 
-    $pdf->SETXY(11, 6);
-    $pdf->CELL(15, 1, 'CONTACTO', 0,);
+    $pdf->SETXY(40, 25);
+    $pdf->CELL(80, 5, 'CONTACTO', 1);
 
-    $pdf->SETXY(11, 7);
-    $pdf->CELL(15, 1, 'CORREO', 0,);
+    $pdf->SETXY(40, 30);
+    $pdf->CELL(80, 5, 'CORREO', 1);
 
-    $pdf->SETXY(11, 9);
-    $pdf->CELL(15, 1, 'REG FISCAL', 0,);
+    $pdf->SETXY(40, 35);
+    $pdf->CELL(80, 5, 'REG FISCAL', 1);
 
     $pdf->Output();
 
