7d1a20bc9df9c8217683424e2abc5a3b29fe3199
Cesar Cornejo <cesarcornejolopez10@gmail.com>
2023-06-26 19:49:31 -0600

    Modificacion a fAcces y creacion de verificacion_sesion

--- public/fAcces.php
+++ public/fAcces.php
@@ -1,8 +1,10 @@
 <?php
-    $rfc = $_POST['rfc'];
-    $pwd = $_POST['pwd'];
+session_start();
 
-    include("../Controlador.php");
+$rfc = $_POST['rfc'];
+$pwd = $_POST['pwd'];
+
+include("../Controlador.php");
 
 $con = Conectar();
 $sql = "SELECT * FROM cuentas WHERE rfc = '$rfc'";
@@ -10,46 +12,45 @@ $result = Ejecutar($con, $sql);
 
 $nFilas = mysqli_num_rows($result);
 
-    if($nFilas == 1){
-        print("\nEl usuario existe\n");
-        $fila=mysqli_fetch_row($result);
-        
-        //condicion de contrasela correcta
-        if($pwd == $fila[1]){
-            
-            //Condicion de cuenta activa
-            if($fila[3] == 1){
-                
-                //Condicion si la contraseña esta sin bloqueo
-                if($fila[4] == 0){
+if ($nFilas == 1) {
+    $fila = mysqli_fetch_row($result);
 
-                    $sql4 = "UPDATE cuentas SET intentos = 0 WHERE rfc = '$rfc';";
-                    $result = Ejecutar($con, $sql4);
+    // Condición de contraseña correcta
+    if ($pwd == $fila[1]) {
 
-                    //Si es Usuario mandar a menuUsuario.html
-                    if($fila[2] == "U"){
-                        header("Location: menuUsuario.html");
-                        exit;
-                    }
-                    //Si es administrador mandar a menuAdmin
-                    else{
-                        header("Location: menuAdmin.html");
-                        exit;
-                    }
+        // Condición de cuenta activa
+        if ($fila[3] == 1) {
+
+            // Condición si la contraseña no está bloqueada
+            if ($fila[4] == 0) {
+
+                $sql4 = "UPDATE cuentas SET intentos = 0 WHERE rfc = '$rfc';";
+                $result = Ejecutar($con, $sql4);
+
+                // Si es usuario, redirigir a menuUsuario.php
+                if ($fila[2] == "U") {
+                    $_SESSION['usuario'] = $rfc;
+                    header("Location: menuUsuario.php");
+                    exit;
+                }
+                // Si es administrador, redirigir a menuAdmin.php
+                else {
+                    $_SESSION['usuario'] = $rfc;
+                    header("Location: menuAdmin.php");
+                    exit;
                 }
             }
-        } else{
-            print("\nContraseña incorrecta\n");
-            $sql2 = "UPDATE cuentas SET intentos = intento+1 WHERE rfc = '$rfc';";
-            $result = Ejecutar($con, $sql2);
-            if($fila[5] == 3) {
-                $sql3 = "UPDATE cuentas SET bloqueo = 1 WHERE rfc = '$rfc';";
-                $result = Ejecutar($con, $sql3);
-            }
         }
-    } else{
-        print("\nEl usuario no existe\n");
+    } else {
+        print("\nContraseña incorrecta\n");
+        $sql2 = "UPDATE cuentas SET intentos = intentos+1 WHERE rfc = '$rfc';";
+        $result = Ejecutar($con, $sql2);
+        if ($fila[5] == 3) {
+            $sql3 = "UPDATE cuentas SET bloqueo = 1 WHERE rfc = '$rfc';";
+            $result = Ejecutar($con, $sql3);
+        }
     }
+}
 
 Desconectar($con);
 ?>
--- verificar_sesion.php
+++ verificar_sesion.php
@@ -4,7 +4,7 @@ session_start();
 // Verificar si la variable de sesión 'usuario' no existe o está vacía
 if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
     // Redirigir a la página de inicio de sesión u otra página adecuada
-    header("Location: index.html");
+    header("Location: ../public/index.php");
     exit; // Detener la ejecución del resto de la página
 }
 
